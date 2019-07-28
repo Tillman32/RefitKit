265509cf0e37314eed95e789078d4c608362db2f
Brandon Tillman <brandon@brandontillman.com>
2019-07-28 10:54:21 -0700

    Update LaunchSettingsFixture to work in Azure DevOps.

--- Interfaces/BattleNet/RefitKit.BattleNet.Tests/LaunchSettingsFixture.cs
+++ Interfaces/BattleNet/RefitKit.BattleNet.Tests/LaunchSettingsFixture.cs
@@ -10,7 +10,17 @@ namespace RefitKit.BattleNet.Tests
     {
         public LaunchSettingsFixture()
         {
-            using (var file = File.OpenText("Properties\\launchSettings.json"))
+            var launchSettings = "Properties\\launchSettings.json";
+
+            // This shouldn't exist when on Azure Pipeline.
+            // For local testing make sure you have a launchSettings.json with env variables.
+            if (!File.Exists(launchSettings))
+            {
+                Console.WriteLine($"{launchSettings} does not exist");
+                return;
+            }
+
+            using (var file = File.OpenText(launchSettings))
             {
                 var reader = new JsonTextReader(file);
                 var jObject = JObject.Load(reader);
